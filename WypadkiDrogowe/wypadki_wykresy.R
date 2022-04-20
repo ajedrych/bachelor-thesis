92af931c3df8a244d710e8a628cf96b39b389976
ajedrych <jedrych.al@gmail.com>
2022-04-20 14:38:07 +0200

    zmiana wykresy drogowe polsa

--- WypadkiDrogowe/wypadki_wykresy.R
+++ WypadkiDrogowe/wypadki_wykresy.R
@@ -2,7 +2,15 @@ library("readxl")
 library(ggplot2)
 options(scipen = 999)
 
+
+#########################################
+####### POLSKA WYPADKI DROGOWE ##########
+#########################################
+
 polska2000 <- read_excel("wypadki_polska_2000-2020.xlsx")
+
+##############################
+# wypadki komunikacyjne polska
 
 ggplot(data = polska2000, mapping = aes(x = rok)) +
   geom_line(aes(y = wypadki), size=1, linetype = 1)+
@@ -12,20 +20,24 @@ ggplot(data = polska2000, mapping = aes(x = rok)) +
        ")+
   xlab("")
 
+###############################
+# ofiary i ranni polska
+
 scaleFactor_polska2000 <- max(polska2000$ranni) / max(polska2000$ofiary)
 
-ggplot(polska2000, aes(x=rok)) +
-  geom_line( aes(y=ranni), size=1) + 
-  geom_line( aes(y=ofiary* scaleFactor_polska2000), size=1, colour = "#F8766D") +
-  scale_y_continuous(
-    name = "Liczba rannych
-    ",
-    # Add a second axis and specify its features
-    sec.axis = sec_axis(~ . /scaleFactor_polska2000, name="Liczba ofiar
-                        "))+ 
+ggplot(polska2000, aes(x = rok))+
+  geom_line(aes(y = ranni, colour = "ranni"), size = 1)+
+  geom_line(aes(y = ofiary * scaleFactor_polska2000, colour = "ofiary"), size = 1)+
+  scale_y_continuous(sec.axis = sec_axis(~./scaleFactor_polska2000, name = "Ofiary
+                                         "))+
+  scale_colour_manual(values = c("black", "#F8766D"))+
+  labs(y = "Ranni
+       ",
+       colour = "")+
   theme_minimal()+
-  theme(legend.title = element_blank())+
-  xlab("")
+  theme(legend.title = element_blank())
+
+
 
 warszawa2010 <- read_excel("wypadki_warszawa_rok_2010-2021.xlsx")
 
